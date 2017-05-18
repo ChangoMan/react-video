64cb555f776ebfb6168b5184b22502cc509f9bad
Hunter Chang <hunter@madwiremedia.com>
2017-05-18 12:13:59 -0600

    minor adjustment to video api

--- src/utils/videoAPI.js
+++ src/utils/videoAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let videoAPI = {
     getVideos : function (playlist) {
-        return axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&playlistId='+playlist+'&key=AIzaSyDsgYSECSyLU3Ae2nLP_RzRIjRohHCOj0I&maxResults=50')
+        return axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId='+playlist+'&key=AIzaSyDsgYSECSyLU3Ae2nLP_RzRIjRohHCOj0I&maxResults=50')
         .then(function (response) {
 
             let videoArray = response.data.items.reduce(function(acc, item) {
